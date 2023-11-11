a211556c2272071e41b93d0e76b24aa2c4aff2de
Joel Pearce <joelpearce1994@gmail.com>
2023-11-11 16:42:41 +0000

    ..

--- learn_japanese_names/urls.py
+++ learn_japanese_names/urls.py
@@ -2,14 +2,19 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from flashcards import views
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('signup/', views.signup, name='signup'),
-    path('', views.home, name='home'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('learn/', views.learn_dashboard, name='learn'),
-    path('review/', views.review_dashboard, name='review'),
+    path("admin/", admin.site.urls),
+    path("signup/", views.signup, name="signup"),
+    path("", views.home, name="home"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("learn/", views.learn_dashboard, name="learn"),
+    path("review/", views.review_dashboard, name="review"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
